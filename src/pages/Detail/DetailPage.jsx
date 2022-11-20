2be7dda0f0cd0bd7eca8733e7c7aaf41837a5bae
Jimeaning <kjimin63@naver.com>
2022-11-20 20:18:28 +0900

    개발: 더미 데이터 넣고 작품 리스트 띄우기

--- src/pages/Detail/DetailPage.jsx
+++ src/pages/Detail/DetailPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import '../../css/DetailPage.css';
+import worksData from '../../worksData';
 
-function DetailPage(props) {
+const DetailPage = () => {
+    const {id} = useParams();
+    const thisWork = worksData.find((work) => {
+        console.log(work);
+
+        return work.id == id;
+    })
+
+
     return (
         <div className='detail'>
             <div id='profile'>
-                <p id='title'>공공 생활을 위한 앱서비스, 투게더</p>
+                <p id='title'>{thisWork.name}</p>
                 <p id='department'>컴퓨터공학과, <span id='year'>2021</span></p>
             </div>
         </div>
---
+++ src/worksData.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        id: 1,
+        name: "공공 생활을 위한 앱서비스, 투게더",
+        description: "Everyone's favorite cat who loves to hate",
+        team: "컴공최강"
+    },
+    {
+        id: 2,
+        name: "그;그림",
+        description:
+            "He bends! He stretches! He even ties in knots, but always returns to his original shape!",
+        team: "메타포"
+    },
+    {
+        id: 3,
+        name: "토끼마켓",
+        description:
+            "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
+        team: "레빗"
+    },
+    {
+        id: 4,
+        name: "딥러닝",
+        description: "You'll get caught up in the crossfire!",
+        team: "알파고"
+    }
+];
