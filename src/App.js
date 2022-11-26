4eb81a093be50244215c7ac94ad5dc31e2f9ec39
Jimeaning <kjimin63@naver.com>
2022-11-26 23:56:26 +0900

    프론트 백 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Router from './components/Router';
 import Hamburger from './components/Dropdown';
 
 function App() {
+  const [message, setMessage] = useState("ttt");
+  useEffect(() => {
+    fetch('/api/test')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  })
   return (
     <div className='App'>
       <Hamburger />
